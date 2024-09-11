cbad60dfe04ff9399be932801bae14c3fe16b0db
chaban <chabchabik@gmail.com>
2024-09-11 12:29:02 +0300

    Updates

---
+++ js/main.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (function() { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./app/js/main.js":
+/*!************************!*\
+  !*** ./app/js/main.js ***!
+  \************************/
+/***/ (function() {
+
+eval("/* const mySum = require('./module/sum.js'); */\r\n\r\n/* import mySum from './module/sum.js' */\r\nconst sum = (a, b) => a + b;\r\nconsole.log(sum(2, 10));\r\nconsole.log(sum(2, 15));\n\n//# sourceURL=webpack://gulp-2024/./app/js/main.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./app/js/main.js"]();
+/******/ 	
+/******/ })()
+;
